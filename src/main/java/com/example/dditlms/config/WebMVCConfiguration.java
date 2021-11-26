5bf64fcbabf98feaa922f176d958c7d185d06c60
show5 <show5116@naver.com>
2021-11-26 14:45:01 +0900

    DropZone

---
+++ src/main/java/com/example/dditlms/config/WebMVCConfiguration.java
@@ -0,0 +1,17 @@
+package com.example.dditlms.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.multipart.commons.CommonsMultipartResolver;
+
+@Configuration
+public class WebMVCConfiguration {
+
+    @Bean
+    public CommonsMultipartResolver multipartResolver(){
+        CommonsMultipartResolver commonsMultipartResolver = new CommonsMultipartResolver();
+        commonsMultipartResolver.setDefaultEncoding("UTF-8");
+        commonsMultipartResolver.setMaxUploadSize(50 * 1024 * 1024);
+        return commonsMultipartResolver;
+    }
+}
