1c6118319406d0b7d3b5a2e5be62ccd3572ecfbb
onyx <bhm1200@naver.com>
2021-11-26 10:50:07 +0900

    QueryDSL setting

--- src/test/java/com/example/dditlms/DditlmsApplicationTests.java
+++ src/test/java/com/example/dditlms/DditlmsApplicationTests.java
@@ -3,11 +3,14 @@ package com.example.dditlms;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+
 @SpringBootTest
 class DditlmsApplicationTests {
+
 
     @Test
     void contextLoads() {
     }
+
 
 }
