f7db7b60e81bdf5c8b406642f844d337fdfcf52f
show5 <show5116@naver.com>
2021-12-14 16:25:41 +0900

    File Upload Util

--- src/main/java/com/example/dditlms/controller/AttachmentController.java
+++ src/main/java/com/example/dditlms/controller/AttachmentController.java
@@ -1,6 +1,7 @@
 package com.example.dditlms.controller;
 
 import com.example.dditlms.security.JwtSecurityService;
+import com.example.dditlms.util.FileUtil;
 import io.jsonwebtoken.ExpiredJwtException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
@@ -8,22 +9,46 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpServletResponse;
-import java.io.File;
+import java.io.*;
+import java.util.Map;
 
 @Controller
 @RequiredArgsConstructor
 public class AttachmentController {
 
-    private final JwtSecurityService jwtSecurityService;
+    private final FileUtil fileUtil;
 
     @GetMapping("/download")
     public void download(HttpServletResponse response,
                          @RequestParam String token) {
-        try{
-            String getToken = jwtSecurityService.getToken(token);
-        }catch (ExpiredJwtException e){
-
+        Map<String,String> map = fileUtil.getToken(token);
+        String path = null;
+        if(map.get("success").equals("Y")){
+            path = map.get("path");
+        }else{
+            //토큰 인증 만료
         }
-
+        File file = new File(path);
+        response.setHeader("Content-Disposition","attachment;filename="+file.getName());
+        FileInputStream fileInputStream = null;
+        OutputStream out = null;
+        try {
+            fileInputStream = new FileInputStream(path);
+            out = response.getOutputStream();
+            int read = 0;
+            byte[] buffer = new byte[1024];
+            while((read = fileInputStream.read(buffer)) != -1){
+                out.write(buffer,0,read);
+            }
+        } catch (FileNotFoundException e) {
+        } catch (IOException e){
+        } finally {
+            try {
+                fileInputStream.close();
+                out.close();
+            } catch (IOException e) {
+            } catch (NullPointerException e){
+            }
+        }
     }
 }
