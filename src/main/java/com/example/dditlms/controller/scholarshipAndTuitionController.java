5fdde9eca7477229bca92a92ec1a7fb78a15e297
show5 <show5116@naver.com>
2021-12-27 10:55:24 +0900

    owlcarousel widget, student page add

--- src/main/java/com/example/dditlms/controller/scholarshipAndTuitionController.java
+++ src/main/java/com/example/dditlms/controller/scholarshipAndTuitionController.java
@@ -3,6 +3,7 @@ package com.example.dditlms.controller;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @RequiredArgsConstructor
@@ -24,8 +25,10 @@ public class scholarshipAndTuitionController {
     }
 
     @GetMapping("/student/scholarshipApplication")
-    public String scholarshipApplication(){
-        return "";
+    public ModelAndView scholarshipApplication(ModelAndView mav){
+
+        mav.setViewName("/pages/scholarshipApplication");
+        return mav;
     }
 
 }
