019ae244aa97dfca50d33468ddab8a2718d077bc
inhwan95 <dldlsghks101@naver.com>
2022-01-03 20:37:37 +0900

    1/3 커커커커커커커 커닝시티

--- src/main/java/com/example/dditlms/controller/AcademicController.java
+++ src/main/java/com/example/dditlms/controller/AcademicController.java
@@ -1,5 +1,7 @@
 package com.example.dditlms.controller;
 
+import com.example.dditlms.domain.common.AcademicStatus;
+import com.example.dditlms.domain.common.ResultStatus;
 import com.example.dditlms.domain.entity.*;
 import com.example.dditlms.domain.repository.EnrolmentRepository;
 import com.example.dditlms.domain.repository.HistoryRepository;
@@ -10,12 +12,14 @@ import com.example.dditlms.util.MemberUtil;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.security.core.parameters.P;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
 import org.springframework.web.servlet.ModelAndView;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.*;
 
 @Controller
@@ -86,6 +90,7 @@ public class AcademicController {
     public ModelAndView cancel(ModelAndView mav){
 
         Map<String, Object> map = new HashMap<>();
+        map.put("hist", "tempAbsence");
         academicService.cancelService(map);
 
         mav.setViewName("redirect:/academic/leave");
@@ -105,7 +110,18 @@ public class AcademicController {
         Student student = MemberUtil.getLoginMember().getStudent();
 
         List<Major> majorList = majorRepository.findAll();
+        Optional<History> historyWrapper = histRepository.findByStudentAndStatusAndResultStatus(student, AcademicStatus.CHANGEMAJOR, ResultStatus.STANDBY);
+        History history = historyWrapper.orElse(null);
 
+        Date date = new Date();
+        int month = date.getMonth()+1;
+        if(month>=1 && month <= 6){
+            mav.addObject("mon", "1학기");
+        } else if(month>=7 && month <= 12){
+            mav.addObject("mon", "2학기");
+        }
+
+        mav.addObject("history", history);
         mav.addObject("student", student);
         mav.addObject("majorList", majorList);
         mav.setViewName("pages/changeMajor");
@@ -114,17 +130,43 @@ public class AcademicController {
 
     @PostMapping("/academic/changePost")
     public ModelAndView changePost(ModelAndView mav){
+        HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes()).getRequest();
+
+        Student student = MemberUtil.getLoginMember().getStudent();
+
+        String reason = request.getParameter("reason");
+        String changeTerm = request.getParameter("change-term");
+
+        Optional<Major> majorWrapper = majorRepository.findById(changeTerm);
+        Major major = majorWrapper.orElse(null);
+
+        History history = History.builder()
+                .aplicationDate(new Date())
+                .status(AcademicStatus.CHANGEMAJOR)
+                .resultStatus(ResultStatus.STANDBY)
+                .note(reason)
+                .student(student)
+                .grade(student.getGrade())
+                .major(major)
+                .build();
+        histRepository.save(history);
 
 
-        mav.setViewName("redirect:/academic/change");
+        mav.setViewName("redirect:/academic/changeMajor");
 
         return mav;
     }
 
-//    @PostMapping("/academic/changeCancel")
-//    public ModelAndView changeCancel(ModelAndView modelAndView){
-//
-//    }
+    @PostMapping("/academic/changeCancel")
+    public ModelAndView changeCancel(ModelAndView mav){
+
+        Map<String, Object> map = new HashMap<>();
+        map.put("hist", "change");
+        academicService.cancelService(map);
+
+        mav.setViewName("redirect:/academic/changeMajor");
+        return mav;
+    }
 
 //    public ModelAndView attendHistory(ModelAndView mav){
 //        Student student = MemberUtil.getLoginMember().getStudent();
---
+++ src/main/java/com/example/dditlms/domain/repository/BbsRepositoryImpl.java
@@ -0,0 +1,59 @@
+package com.example.dditlms.domain.repository;
+
+import com.example.dditlms.domain.common.BoardCategory;
+import com.example.dditlms.domain.dto.BbsDTO;
+import com.example.dditlms.domain.dto.QBbsDTO;
+import com.example.dditlms.domain.entity.Bbs;
+import com.example.dditlms.domain.entity.QBbs;
+import com.querydsl.core.QueryResults;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
+
+import javax.persistence.EntityManager;
+import java.util.List;
+
+import static com.example.dditlms.domain.entity.QBbs.bbs;
+
+@Slf4j
+public class BbsRepositoryImpl implements BbsRepositoryCustom{
+
+    private final JPAQueryFactory queryFactory;
+
+    public BbsRepositoryImpl(EntityManager entityManager){
+        this.queryFactory = new JPAQueryFactory(entityManager);
+    }
+
+    @Override
+    public Page<BbsDTO> pageWithFree(Pageable pageable, BoardCategory boardCategory){
+
+
+        QueryResults<BbsDTO> results = queryFactory
+                .selectDistinct(new QBbsDTO(bbs.idx,
+                        bbs.member,
+                        bbs.title,
+                        bbs.content,
+                        bbs.bbsDate,
+                        bbs.bbsCnt,
+                        bbs.category,
+                        bbs.useAt,
+                        bbs.atchmnflId
+                        ))
+                .from(bbs)
+                .where(bbs.category.eq(boardCategory))
+                .offset(pageable.getOffset())
+                .limit(pageable.getPageSize())
+                .fetchResults();
+
+
+        List<BbsDTO> content = results.getResults();
+
+        long total = results.getTotal();
+
+        return new PageImpl<>(content, pageable, total);
+    }
+
+
+}
